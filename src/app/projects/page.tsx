d1c9208107a1017b7a1101949441f4e058742d97
SERAFINO <ymontenegro@estudiantec.cr>
2025-09-02 00:54:09 -0600

    Se agrega e integran cardTrabajo para visualizarlas verticalmente en un listado de trabajos del modal.
    Estilos de la pagina projects para el titulo, contenedor de cursos, etc. Centrado

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "../../styles/projects.css";
 import React, {useState} from "react";
+import CardTrabajo from "@/components/CardTrabajo";
 import FiltrosTrabajos from "@/components/FiltrosTrabajo";
 import ModalTrabajos from "@/components/ModalTrabajos";
 import Carousel from "@/components/Carousel";
@@ -9,10 +10,71 @@ import CardCurso from "@/components/CardCurso";
 export default function ProjectsPage() {
   const [cursoSeleccionado, setCursoSeleccionado] = useState<null | { codigo: string; nombre: string; }>(null);
 
+    // Mock data para trabajos, pa ver como se ve
+    const trabajosEjemplo = [
+    {
+      nombre: "Sitio Web Personal",
+      tipo: "Proyecto",
+      descripcion: "Desarrollo de un sitio web personal usando React y CSS.",
+      fecha: "2025-05-10",
+      tecnologias: ["React", "CSS", "Vercel"],
+      repoUrl: "https://github.com/",
+      deployUrl: "https://sitio-increible.com",
+    },
+    {
+      nombre: "Laboratorio de HTML",
+      tipo: "Laboratorio",
+      descripcion: "Práctica de etiquetas semánticas y estructura básica HTML.",
+      fecha: "2025-03-15",
+      tecnologias: ["HTML", "CSS"],
+      repoUrl: "",
+      deployUrl: "",
+    },
+        {
+      nombre: "Laboratorio de HTML",
+      tipo: "Laboratorio",
+      descripcion: "Práctica de etiquetas semánticas y estructura básica HTML.",
+      fecha: "2025-03-15",
+      tecnologias: ["HTML", "CSS"],
+      repoUrl: "",
+      deployUrl: "",
+    },
+        {
+      nombre: "Laboratorio de HTML",
+      tipo: "Laboratorio",
+      descripcion: "Práctica de etiquetas semánticas y estructura básica HTML.",
+      fecha: "2025-03-15",
+      tecnologias: ["HTML", "CSS"],
+      repoUrl: "",
+      deployUrl: "",
+    },
+        {
+      nombre: "Laboratorio de HTML",
+      tipo: "Laboratorio",
+      descripcion: "Práctica de etiquetas semánticas y estructura básica HTML.",
+      fecha: "2025-03-15",
+      tecnologias: ["HTML", "CSS"],
+      repoUrl: "",
+      deployUrl: "",
+    },
+        {
+      nombre: "Laboratorio de HTML",
+      tipo: "Laboratorio",
+      descripcion: "Práctica de etiquetas semánticas y estructura básica HTML.",
+      fecha: "2025-03-15",
+      tecnologias: ["HTML", "CSS"],
+      repoUrl: "",
+      deployUrl: "",
+    },
+    
+  ];
+
   return (
     <main className="projects-container">
-      <h1>Proyectos</h1>
-      <p>Página para proyectos realizados en los cursos</p>
+      <h1 className="projects-title">Proyectos</h1>
+      <p className="project-desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod, urna eu tincidunt consectetur, nisi nisl aliquam enim, nec dictum sem urna at sapien.
+      </p>
       <Carousel>
         {/* card estatica, luego dinamica con fetch a backend */}
         <CardCurso
@@ -30,12 +92,15 @@ export default function ProjectsPage() {
       <ModalTrabajos open={!!cursoSeleccionado} onClose={() => setCursoSeleccionado(null)}>
         <h2>Trabajos del Curso {cursoSeleccionado?.nombre} - {cursoSeleccionado?.codigo}</h2>
         <section className="filtros-section">
+          <h3 className="filtros-title">Filtrar por: </h3>
           <FiltrosTrabajos />
         </section>
         <section className="trabajos-list">
-
+          {/* creacion dinamoca de cards de trabajos */}
+          {trabajosEjemplo.map((trabajo, idx) => (
+            <CardTrabajo key={idx} {...trabajo} />
+          ))}
         </section>
-        {/* cardsTrabajo y filtros */}
       </ModalTrabajos>
     </main>
   );
--- src/components/FiltrosTrabajo.tsx
+++ src/components/FiltrosTrabajo.tsx
@@ -2,30 +2,30 @@ import React from "react";
 import "../styles/components.css";
 
 export default function FiltrosTrabajos() {
-  return (
-    <section className="filtros-trabajos">
-      <form>
-        <label>
-          Tipo de evaluación:
-          <select>
-            <option value="">Todos</option>
-            <option value="laboratorio">Laboratorio</option>
-            <option value="tarea">Tarea</option>
-            <option value="proyecto">Proyecto</option>
-            <option value="investigacion">Investigación</option>
-            <option value="examen">Examen</option>
-            <option value="otro">Otro</option>
-          </select>
-        </label>
-        <label>
-          Tecnología:
-          <input type="text" placeholder="Ej: .Net, NodeJS..." />
-        </label>
-        <label>
-          Fecha:
-          <input type="date" />
-        </label>
-      </form>
-    </section>
-  );
+    return (
+        <section className="filtros-trabajos">
+            <form>
+                <label>
+                Tipo de evaluación:
+                <select>
+                    <option value="">Todos</option>
+                    <option value="laboratorio">Laboratorio</option>
+                    <option value="tarea">Tarea</option>
+                    <option value="proyecto">Proyecto</option>
+                    <option value="investigacion">Investigación</option>
+                    <option value="examen">Examen</option>
+                    <option value="otro">Otro</option>
+                </select>
+                </label>
+                <label>
+                Tecnología:
+                <input type="text" placeholder="Ej: .Net, NodeJS..." />
+                </label>
+                <label>
+                Fecha:
+                <input type="date" />
+                </label>
+            </form>
+        </section>
+    );
 }
